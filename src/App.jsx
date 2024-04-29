6cbaf79bb9526b2121377ef91a4c14cd6d634ff1
Diegoalias <32991248+DiegoAlias@users.noreply.github.com>
2024-04-29 07:38:55 -0300

    changes App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import React from "react";
 import CanvasComponent from "./components/ClinicalComponents/CapturedPhotoCanvas.jsx";
-import { Login } from "./components/login.jsx";
 
 const App = () => {
   return (
     <div>
-      <Login />
       <CanvasComponent arrowColor="red" />
     </div>
   );
